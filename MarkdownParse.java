cb05f22b9c82efd0bcccac345a8483712464bd52
heihaheihahello <97714936+heihaheihahello@users.noreply.github.com>
2022-01-27 08:53:45 -0800

    bug-fix-1(new3)
    fixed input: test file with no link, no [] and ().
    bug fixed: program will show "StringIndexOutOfBoundsException" error if there is no link in test file.
    after fixed: the program will output [] if there is no links, no [] and () in test file.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,13 +10,19 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
+        //System.out.println("Before loop currentIndex: " + currentIndex);
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if(nextOpenBracket == -1){
+                break;
+            } 
+            //System.out.println("Beggining of loop currentIndex: " + currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            currentIndex = closeParen + 1; //one after closed paren
+            //System.out.println("current index: " + currentIndex);
         }
         return toReturn;
     }

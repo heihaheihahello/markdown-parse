02cde49275119c6d9dd5992a49833cc936b2da67
heihaheihahello <97714936+heihaheihahello@users.noreply.github.com>
2022-01-27 18:48:42 -0800

    first try for assert (test for junit)

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,3 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
 public class MarkdownParseTest {
-    
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void links() throws IOException {
+        ArrayList<String> output = new ArrayList<> ();
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        output.add("https://something.com");
+        output.add("some-page.html");
+        assertEquals("test for getlinks", output, links);
+
+        
+    }
 }
